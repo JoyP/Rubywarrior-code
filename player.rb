dfb7e0cd5d582f80b7373834272aeaf26c6e7b36
JoyP <joypratt820@gmail.com>
2014-01-05 16:08:36 -0600

    Update player.rb

--- player.rb
+++ player.rb
@@ -1,26 +1,54 @@
+
 class Player
   def play_turn(warrior)
-    # cool code goes here
     @health ||= 20
-    if attr_writer :arrior.feel.captive?
-      warrior.rescue!
-      @hero=true
-    elsif warrior.feel(:backward).captive?
-      warrior.rescue! :backward
-      @hero=true
-    elsif warrior.feel.empty? and warrior.health < 20 and warrior.health >= @health
-      warrior.rest!
-    elsif warrior.feel.empty? and warrior.health < 15 and warrior.health < @health
-      warrior.walk! :backward
-    elsif warrior.feel.empty?
-      if @hero
-        warrior.walk!
+    
+    if warrior.feel.wall?
+      warrior.pivot!
+    else 
+      if rescue_captives(warrior)
+      elsif heal_or_retreat(warrior)
+      elsif attack_enemy(warrior)
       else
-        warrior.walk! :backward
+        warrior.walk!
+
       end
-    else
-      warrior.attack!
     end
     @health = warrior.health
   end
+  
+  def rescue_captives(warrior)
+    # rescue captives, if there are any in a space next to warrior
+    if warrior.look[0].captive?
+        warrior.rescue! 
+        @hero=true
+    elsif warrior.feel(:backward).captive?
+        warrior.rescue! :backward
+        @hero=true
+    end
+  end
+  
+  def heal_or_retreat(warrior)
+    if warrior.feel.empty? and warrior.health < 20 and warrior.health >= @health 
+        warrior.rest!
+    elsif warrior.feel.empty? and warrior.health < 15 and warrior.health < @health 
+        warrior.walk! :backward
+    end
+  end
+  
+  def attack_enemy(warrior)
+    if enemy_position(warrior) < captive_position(warrior)
+      warrior.shoot!
+    end
+  end
+  
+  def enemy_position(warrior)
+    warrior.look.find_index {|space| space.enemy?} or 4
+  end
+  
+  def captive_position(warrior)
+    warrior.look.find_index {|space| space.captive?} or 4
+  end
+  
 end
+  
