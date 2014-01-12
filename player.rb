62b8db9bbf3379c840d600d0ad5822186d61b6fb
JoyP <JoyP@users.noreply.github.com>
2014-01-12 15:07:15 -0600

    Level 9 refactored

--- player.rb
+++ player.rb
@@ -1,24 +1,27 @@
 class Player
+  attr_accessor :warrior
+  
   def play_turn(warrior)
+    self.warrior = warrior
     @health ||= 20
     
     if warrior.feel.wall?
       warrior.pivot!
-      @feltwall=true
+      @feltwall = true
     else
       if warrior.feel(:backward).enemy?
         warrior.pivot!
-      elsif rescue_captives(warrior)
-      elsif heal_or_retreat(warrior)
-      elsif attack_enemy(warrior)
+      elsif rescue_captives
+      elsif heal_or_retreat
+      elsif attack_enemy
       else 
-        walk_or_pivot(warrior)
+        walk_or_pivot
       end
     end
     @health = warrior.health
   end
   
-  def rescue_captives(warrior)
+  def rescue_captives
     # rescue captives, if there are any in a space next to warrior
     if warrior.look[0].captive?
         warrior.rescue! 
@@ -27,7 +30,7 @@ class Player
     end
   end
   
-  def heal_or_retreat(warrior)
+  def heal_or_retreat
     if warrior.feel.empty?
       if warrior.health < 20 and warrior.health >= @health 
         warrior.rest!
@@ -37,23 +40,23 @@ class Player
     end
   end
   
-  def attack_enemy(warrior)
+  def attack_enemy
     if warrior.feel.enemy?
       warrior.attack!
-    elsif enemy_position(warrior) < captive_position(warrior)
+    elsif enemy_position < captive_position
       warrior.shoot!
     end
   end
   
-  def enemy_position(warrior)
+  def enemy_position
     warrior.look.find_index {|space| space.enemy?} or 4
   end
   
-  def captive_position(warrior)
+  def captive_position
     warrior.look.find_index {|space| space.captive?} or 4
   end
   
-  def walk_or_pivot(warrior)
+  def walk_or_pivot
     if warrior.feel.stairs? and !@feltwall
       warrior.pivot!
     else 
@@ -61,3 +64,5 @@ class Player
     end
   end      
 end
+
+  
